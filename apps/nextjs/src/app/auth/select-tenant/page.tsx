a13f6fa26b3eea63856007c3e884caceb9f7528e
CllDMR <celal.demirx@gmail.com>
2023-08-25 19:34:23 +0300

    feat: form validation

--- apps/nextjs/src/app/auth/select-tenant/page.tsx
+++ apps/nextjs/src/app/auth/select-tenant/page.tsx
@@ -1,22 +1,26 @@
 import { revalidatePath } from "next/cache";
+import { zfd } from "zod-form-data";
 
-import { db, schema } from "@acme/db";
-import type { InsertTenant } from "@acme/db/schema/auth";
+import { db, desc, schema } from "@acme/db";
 
 export default async function SelectTenantPage() {
-  const tenants = await db.select().from(schema.tenant);
+  const tenants = await db
+    .select()
+    .from(schema.tenant)
+    .orderBy(desc(schema.tenant.name));
 
   const action = async (formData: FormData) => {
     "use server";
+    const validationSchema = zfd.formData({
+      name: zfd.text(),
+    });
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const values: InsertTenant = JSON.parse(
-      JSON.stringify(Object.fromEntries(formData)),
-    );
+    const result = validationSchema.safeParse(formData);
 
-    await db.insert(schema.tenant).values(values);
-
-    revalidatePath("/auth/select-tenant");
+    if (result.success) {
+      await db.insert(schema.tenant).values(result.data);
+      revalidatePath("/auth/select-tenant");
+    } else console.error(result.error.flatten());
   };
 
   return (
@@ -24,7 +28,7 @@ export default async function SelectTenantPage() {
       <form className="" action={action}>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <input type="text" id="name" />
         </div>
 
         <button type="submit">Send</button>
