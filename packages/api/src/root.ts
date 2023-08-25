10abc9be87c789e128913635f8fb30c45c44330b
CllDMR <celal.demirx@gmail.com>
2023-08-25 17:57:45 +0300

    feat: tenant api

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,12 @@
 import { authRouter } from "./router/auth";
 import { postRouter } from "./router/post";
+import { tenantRouter } from "./router/tenant";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   post: postRouter,
+  tenant: tenantRouter,
 });
 
 // export type definition of API
---
+++ packages/api/src/router/tenant.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+import { desc, eq, schema } from "@acme/db";
+
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+
+export const tenantRouter = createTRPCRouter({
+  list: publicProcedure.query(({ ctx }) =>
+    ctx.db.select().from(schema.tenant).orderBy(desc(schema.tenant.id)),
+  ),
+
+  get: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) =>
+      ctx.db
+        .select()
+        .from(schema.tenant)
+        .where(eq(schema.tenant.id, input.id))
+        .then((a) => a[0]),
+    ),
+
+  create: protectedProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(({ ctx, input }) => ctx.db.insert(schema.tenant).values(input)),
+
+  update: protectedProcedure
+    .input(z.object({ id: z.string(), name: z.string().min(1).optional() }))
+    .mutation(
+      ({ ctx, input }) =>
+        void ctx.db
+          .update(schema.tenant)
+          .set(input)
+          .where(eq(schema.tenant.id, input.id)),
+    ),
+
+  delete: publicProcedure
+    .input(z.string())
+    .mutation(
+      ({ ctx, input }) =>
+        void ctx.db.delete(schema.tenant).where(eq(schema.tenant.id, input)),
+    ),
+});
