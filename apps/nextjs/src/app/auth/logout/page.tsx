03c21cb83460379dab974c3d5efdca0937f88318
CllDMR <celal.demirx@gmail.com>
2023-08-27 05:47:46 +0300

    fix: logout callback

--- apps/nextjs/src/app/auth/logout/page.tsx
+++ apps/nextjs/src/app/auth/logout/page.tsx
@@ -3,6 +3,16 @@ import { CSRF_experimental } from "@acme/auth";
 export default function LogoutPage() {
   return (
     <form className="" action={`/api/auth/signout`} method="post">
+      <div className="hidden">
+        <input
+          hidden
+          type="text"
+          name="callbackUrl"
+          id="callbackUrl"
+          defaultValue="/"
+        />
+      </div>
+
       <button type="submit">Logout</button>
       <CSRF_experimental />
     </form>
