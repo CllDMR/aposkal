0e001b40589bd4cf5b37c92a14300eb69231d2f4
CllDMR <celal.demirx@gmail.com>
2023-08-30 17:08:37 +0300

    feat: removed post list + create form from posts page

--- apps/nextjs/src/app/(protected)/posts/page.tsx
+++ apps/nextjs/src/app/(protected)/posts/page.tsx
@@ -2,8 +2,6 @@ import { authOptions, getServerSession } from "@acme/auth";
 import { db, desc, eq, schema } from "@acme/db";
 
 import { Main } from "~/components/atoms/Main";
-import { PostCreateForm } from "~/components/organisms/post/PostCreateForm";
-import { PostList } from "~/components/organisms/post/PostList";
 import { PostTable } from "~/components/organisms/post/PostTable";
 
 export default async function PostsPage() {
@@ -20,11 +18,7 @@ export default async function PostsPage() {
     <Main>
       <h1>Posts Page</h1>
 
-      <PostCreateForm />
-
       <PostTable posts={posts} />
-
-      <PostList posts={posts} />
     </Main>
   );
 }
