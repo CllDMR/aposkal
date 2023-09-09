7bfa1ad3b54cecb036404c8884165b1ac36d2058
CllDMR <celal.demirx@gmail.com>
2023-09-09 14:59:12 +0300

    feat: cut unused tailwind colors

--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -1,8 +1,25 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
   content: [""],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      primary: colors.indigo,
+      secondary: colors.emerald,
+      accent: colors.amber,
+      success: colors.teal,
+      info: colors.sky,
+      warning: colors.orange,
+      danger: colors.red,
+      disabled: colors.slate,
+      yellow: colors.yellow,
+    },
     extend: {},
   },
   plugins: [],
