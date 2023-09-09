46ac481d1dbc3d4a7b3288826506c8dee0e41a76
CllDMR <celal.demirx@gmail.com>
2023-09-09 15:00:12 +0300

    feat: new button

--- apps/nextjs/src/components/molecules/button/index.tsx
+++ apps/nextjs/src/components/molecules/button/index.tsx
@@ -1,17 +1,77 @@
-import type { ButtonHTMLAttributes, DetailedHTMLProps, FC } from "react";
+import type { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
+import clsx from "clsx";
 
-type ButtonProps = DetailedHTMLProps<
+enum ButtonVariant {
+  ERROR,
+  WARNING,
+  SUCCESS,
+  INFO,
+}
+
+enum ButtonSize {
+  LARGE,
+  SMALL,
+}
+
+const SIZE_MAPS: Record<ButtonSize, string> = {
+  [ButtonSize.SMALL]: "px-2.5 text-xs",
+  [ButtonSize.LARGE]: "px-3 text-sm",
+};
+
+const VARIANT_MAPS: Record<ButtonVariant, string> = {
+  [ButtonVariant.SUCCESS]:
+    "bg-success-100 text-success-800 hover:bg-success-500 focus-visible:outline-success-600",
+  [ButtonVariant.INFO]:
+    "bg-info-100 text-info-800 hover:bg-info-500 focus-visible:outline-info-600",
+  [ButtonVariant.WARNING]:
+    "bg-warning-100 text-warning-800 hover:bg-warning-500 focus-visible:outline-warning-600",
+  [ButtonVariant.ERROR]:
+    "bg-danger-100 text-danger-800 hover:bg-danger-500 focus-visible:outline-danger-600",
+};
+
+type HTMLButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 >;
 
-export const Button: FC<ButtonProps> = ({ children, ...rest }) => {
+interface ButtonProps {
+  children?: ReactNode;
+
+  // UI
+  type?: HTMLButtonProps["type"];
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+
+  // State
+  disabled?: HTMLButtonProps["disabled"];
+  // isLoading?: boolean;
+
+  //Functionality
+  onClick?: HTMLButtonProps["onClick"];
+}
+
+export function Button({
+  children,
+  type,
+  variant = ButtonVariant.INFO,
+  size = ButtonSize.SMALL,
+  disabled = false,
+  onClick,
+}: ButtonProps) {
+  const css = clsx(
+    "inline-flex items-center justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:bg-disabled-600",
+    VARIANT_MAPS[variant],
+    SIZE_MAPS[size],
+
+    {},
+  );
+
   return (
-    <button
-      className="inline-flex items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-slate-600"
-      {...rest}
-    >
+    <button className={css} type={type} disabled={disabled} onClick={onClick}>
       {children}
     </button>
   );
-};
+}
+
+Button.variant = ButtonVariant;
+Button.size = ButtonSize;
