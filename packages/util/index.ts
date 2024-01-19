b03a448dcfa0885d2b628f17f2c507a5c0b42c78
CllDMR <celal.demirx@gmail.com>
2024-01-19 16:06:08 +0300

    feat: moved code into proper packages

--- packages/util/index.ts
+++ packages/util/index.ts
@@ -1,1 +1,5 @@
-export {};
+export { api } from "./src/api";
+export { getBaseAuthUrl, getBaseUrl } from "./src/get-base-url";
+export { parseRequestBody } from "./src/parseBody";
+export { sidebarItems } from "./src/sidebaritems";
+export { tryCatch } from "./src/try";
---
+++ packages/util/src/get-base-url.ts
@@ -0,0 +1,14 @@
+import { env } from "@acme/env";
+
+export const getBaseAuthUrl = () => {
+  return env.NEXTAUTH_URL;
+};
+
+export const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.NEXT_PUBLIC_MUHASEBE_BASE_URL)
+    return process.env.NEXT_PUBLIC_MUHASEBE_BASE_URL;
+  if (process.env.VERCEL_URL) return process.env.VERCEL_URL; // SSR should use vercel url
+
+  return `http://localhost:${process.env.MUHASEBE_PORT}`; // dev SSR should use localhost
+};
